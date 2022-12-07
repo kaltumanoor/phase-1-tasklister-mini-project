6d7cb9546aefaed6318a4c1cbb210909c4983c61
kaltuma <kaltumanoor370@gmail.com>
2022-12-07 15:11:51 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  document.querySelector("form").addEventListener('submit',(e)=>{
+    e.preventDefault()
+    createTodo(e.target.children[1].value)    
+
+    
+  })
+
+
 });
+function createTodo(todo){
+
+    let x = document.createElement('button')
+    let lis = document.createElement("li")
+    lis.textContent = `${todo} `
+    lis.appendChild(x)
+    x.innerHTML = ' x'
+    x.addEventListener(
+      'click', () => {
+        lis.remove()
+      }) 
+
+    document.querySelector("#tasks").appendChild(lis);
+
+}
